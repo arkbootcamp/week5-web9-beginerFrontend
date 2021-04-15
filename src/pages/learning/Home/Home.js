c63d3fddb1cdc9b9da42b496f7475bca6d625e6d
bagus tri harjanto <bagustri15@gmail.com>
2021-04-15 15:49:05 +0700

    Basic React App

---
+++ src/pages/learning/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <h1>Home Page !</h1>
+        <h2 className="header">Style With BasicReact.css</h2>
+      </>
+    );
+  }
+}
+
+export default Home;
---
+++ src/pages/learning/MovieDetail/MovieDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MovieDetail extends Component {
+  render() {
+    return (
+      <>
+        <h1>MovieDetail Page !</h1>
+      </>
+    );
+  }
+}
+
+export default MovieDetail;
